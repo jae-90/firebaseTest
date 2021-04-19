4b27b65a23bd87662147b7e87b3b3063e2ab3773
Jae <jaewoolee@costco.com>
2021-04-19 16:22:58 -0700

    App with default 'google-services.json' and try to switch programmatically.

--- app/src/main/java/com/jw/firebasetest/MainActivity.kt
+++ app/src/main/java/com/jw/firebasetest/MainActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
@@ -14,10 +16,10 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG: String = MainActivity::class.java.simpleName
 
-    val remoteConfig = Firebase.remoteConfig
-    val analytics = Firebase.analytics
+    private val remoteConfig = Firebase.remoteConfig
+    private val analytics = Firebase.analytics
 
-    val configSettings = remoteConfigSettings {
+    private val configSettings = remoteConfigSettings {
         minimumFetchIntervalInSeconds = 5
     }
 
@@ -42,5 +44,31 @@ class MainActivity : AppCompatActivity() {
                         findViewById<TextView>(R.id.text_view).text = "Fetch Failed"
                     }
                 }
+
+        // when the textview in the middle of the screen is tapped, try to initialize a different project.
+        // doing this crashes with:
+        // java.lang.IllegalStateException: FirebaseApp name [DEFAULT] already exists!
+        //  at com.google.android.gms.common.internal.Preconditions.checkState(com.google.android.gms:play-services-basement@@17.2.1:29)
+        //  at com.google.firebase.FirebaseApp.initializeApp(FirebaseApp.java:294)
+        //  at com.google.firebase.FirebaseApp.initializeApp(FirebaseApp.java:267)
+        findViewById<TextView>(R.id.text_view).setOnClickListener {
+            initializeSecondSetOfFirebaseProjectProgrammatically()
+        }
     }
+
+    private fun initializeSecondSetOfFirebaseProjectProgrammatically() {
+        FirebaseApp.initializeApp(this, option_proj2)
+    }
+
+    // default project option. (this got initialized automatically by 'google-services.json')
+    private val option_proj1 = FirebaseOptions.Builder()
+        .setApplicationId("1:1080837297219:android:5ba48abfe65978cb1bf0f6")
+        .setProjectId("jw-firebase-proj")
+        .setApiKey("AIzaSyA38lYx2-GWiFm311Haoqf5TDZUBkeevWA").build()
+
+    // second set of project option.
+    private val option_proj2 = FirebaseOptions.Builder()
+        .setApplicationId("1:955949062470:android:3f34125cf9dd3cd065ad47")
+        .setProjectId("jw-firebase-proj2")
+        .setApiKey("AIzaSyDyvv_RFmd5iQzwJTodFZ0_DlzLFi8VA8M").build()
 }
